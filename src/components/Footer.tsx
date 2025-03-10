b79cb6c8fd2e90316206fa00e21875b09ba2adda
Diego Izac <diegoizac@hotmail.com>
2025-03-10 17:11:04 -0300

    Salvar alterações antes de inicializar o Gitflow

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,7 +6,7 @@ interface FooterProps {
 
 const Footer: React.FC<FooterProps> = () => {
   return (
-    <footer className="text-white py-6 fixed bottom-0 w-full" id="row-footer">
+    <footer className="text-black py-6 fixed bottom-0 w-full" id="row-footer">
       <div className="container mx-auto text-center">
         <div className="social-icons flex justify-center space-x-6 mb-4">
           {/* Facebook */}
@@ -14,7 +14,7 @@ const Footer: React.FC<FooterProps> = () => {
             href="https://www.facebook.com/christianitatis"
             target="_blank"
             rel="noopener noreferrer"
-            className="text-gray-300 hover:text-white text-2xl animate fade-in animation-time-1s delay-08s"
+            className="text-blue-600 hover:text-blue-800 text-2xl animate fade-in animation-time-1s delay-08s"
           >
             <i className="fa-brands fa-facebook"></i>
           </a>
@@ -23,7 +23,7 @@ const Footer: React.FC<FooterProps> = () => {
             href="https://www.youtube.com/@christianitatis2106"
             target="_blank"
             rel="noopener noreferrer"
-            className="text-gray-300 hover:text-white text-2xl animate fade-in animation-time-1s delay-06s"
+            className="text-red-600 hover:text-red-800 text-2xl animate fade-in animation-time-1s delay-06s"
           >
             <i className="fa-brands fa-youtube"></i>
           </a>
@@ -32,18 +32,18 @@ const Footer: React.FC<FooterProps> = () => {
             href="https://www.instagram.com/christianitatis"
             target="_blank"
             rel="noopener noreferrer"
-            className="text-gray-300 hover:text-white text-2xl animate fade-in animation-time-1s delay-06s"
+            className="text-pink-600 hover:text-pink-800 text-2xl animate fade-in animation-time-1s delay-06s"
           >
             <i className="fa-brands fa-instagram"></i>
           </a>
         </div>
-        <p className="text-gray-300">
+        <p className="text-gray-700">
           © 2025 All Rights Reserved. Christianitatis |
           <a
             href="https://2dlcompany.com.br"
             target="_blank"
             rel="noopener noreferrer"
-            className="text-blue-400 hover:text-blue-300 ml-1 animate fade-in animation-time-1s delay-1s"
+            className="text-blue-600 hover:text-blue-500 ml-1 animate fade-in animation-time-1s delay-1s"
           >
             Feito com 💙 por 2DL Company
           </a>
