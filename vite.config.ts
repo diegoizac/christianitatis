98e9f22aacc3148cfe0eaf329eb3a95e0c3e840f
Diego Izac <diegoizac@hotmail.com>
2025-03-13 10:51:43 -0300

    Fix build issues

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/",
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        { src: "src/assets/images/*", dest: "assets/images" },
+        { src: "src/assets/animations/*", dest: "assets/animations" },
+      ],
+    }),
+  ],
   optimizeDeps: {
     exclude: ["lucide-react"],
   },
